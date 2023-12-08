d0b8bb20dcede19a6b359cd092d7882196a88649
karoke <karolke99@gmail.com>
2023-12-08 15:17:12 +0100

    in progress

---
+++ perceptron-openmp-test.c
@@ -0,0 +1,224 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <omp.h>
+
+// To jest program z podziałem na 8 perceptronów i dla każdej liczby wątków ma ten sam wynik więc jest ok.
+
+
+#define FEATURES 8  // Liczba cech (atrybutów) w danych
+
+#define NUM_PERCEPTRONS 8
+
+// Struktura reprezentująca pojedynczy punkt danych
+typedef struct {
+    double features[FEATURES];
+    int label;
+} DataPoint;
+
+// Struktura reprezentująca perceptron
+typedef struct {
+    double weights[FEATURES];
+    double bias;
+} Perceptron;
+
+// Funkcja sigmoidalna
+double sigmoid(double x) {
+    return 1.0 / (1.0 + exp(-x));
+}
+
+// Funkcja ucząca perceptron na pojedynczym punkcie danych
+void trainPerceptron(Perceptron *perceptron, DataPoint data, double learningRate) {
+    double weightedSum = perceptron->bias;
+    for (int i = 0; i < FEATURES; i++) {
+        weightedSum += perceptron->weights[i] * data.features[i];
+    }
+
+    double prediction = sigmoid(weightedSum);
+    double error = data.label - prediction;
+
+    #pragma omp critical 
+    {
+        perceptron->bias += learningRate * error * prediction * (1.0 - prediction);
+
+        for (int i = 0; i < FEATURES; i++) {
+            perceptron->weights[i] += learningRate * error * prediction * (1.0 - prediction) * data.features[i];
+        }
+    }
+}
+
+// Funkcja klasyfikująca dane za pomocą perceptronu
+int predict(Perceptron perceptron, double *features) {
+    double weightedSum = perceptron.bias;
+    for (int i = 0; i < FEATURES; i++) {
+        weightedSum += perceptron.weights[i] * features[i];
+    }
+
+    double prediction = sigmoid(weightedSum);
+    return (prediction >= 0.5) ? 1 : 0;
+}
+
+// Funkcja do losowego tasowania danych
+void shuffleData(DataPoint *dataSet, int dataSize) {
+    for (int i = dataSize - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+
+        // Zamień elementy i j miejscami
+        DataPoint temp = dataSet[i];
+        dataSet[i] = dataSet[j];
+        dataSet[j] = temp;
+    }
+}
+
+// Funkcja do obliczania błędu na zbiorze danych
+double calculateError(Perceptron perceptron, DataPoint *dataSet, int dataSize) {
+    double totalError = 0.0;
+    for (int i = 0; i < dataSize; i++) {
+        double predictedLabel = predict(perceptron, dataSet[i].features);
+        totalError += fabs(dataSet[i].label - predictedLabel);
+    }
+    return totalError / dataSize;
+}
+
+// Funkcja do normalizacji danych zgodnie ze standard scalerem
+void standardScaler(DataPoint *dataSet, int dataSize) {
+    double mean[FEATURES] = {0.0};
+    double variance[FEATURES] = {0.0};
+
+    // Oblicz średnią dla każdej cechy
+    for (int i = 0; i < dataSize; i++) {
+        for (int j = 0; j < FEATURES; j++) {
+            mean[j] += dataSet[i].features[j];
+        }
+    }
+
+    for (int j = 0; j < FEATURES; j++) {
+        mean[j] /= dataSize;
+    }
+
+    // Oblicz wariancję dla każdej cechy
+    for (int i = 0; i < dataSize; i++) {
+        for (int j = 0; j < FEATURES; j++) {
+            variance[j] += pow(dataSet[i].features[j] - mean[j], 2);
+        }
+    }
+
+    for (int j = 0; j < FEATURES; j++) {
+        variance[j] /= dataSize;
+        variance[j] = sqrt(variance[j]);
+    }
+
+    // Zastosuj standard scaler do każdej cechy
+    for (int i = 0; i < dataSize; i++) {
+        for (int j = 0; j < FEATURES; j++) {
+            dataSet[i].features[j] = (dataSet[i].features[j] - mean[j]) / variance[j];
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    // Inicjalizacja generatora liczb losowych
+    srand(42);
+
+    int num_threads = 4;  // Domyślna liczba wątków
+    if (argc > 1) {
+        num_threads = atoi(argv[1]);
+    }
+    omp_set_num_threads(num_threads);
+
+    FILE *file = fopen("diab.csv", "r");
+    if (file == NULL) {
+        perror("Unable to open file");
+        return 1;
+    }
+
+    // Wczytanie danych z pliku
+    int dataSize = 768;  // Załóżmy, że plik zawiera 768 rekordów danych
+    DataPoint *dataSet = malloc(dataSize * sizeof(DataPoint));
+
+    // Pominięcie pierwszej linii (nagłówek)
+    char buffer[256];
+    fgets(buffer, sizeof(buffer), file);
+
+    for (int i = 0; i < dataSize; i++) {
+        for (int j = 0; j < FEATURES; j++) {
+            fscanf(file, "%lf,", &dataSet[i].features[j]);
+        }
+        fscanf(file, "%d", &dataSet[i].label);
+    }
+
+    fclose(file);
+
+    int start = omp_get_wtime();
+
+    // Normalizacja danych zgodnie ze standard scalerem
+    standardScaler(dataSet, dataSize);
+
+    // Tasowanie danych
+    shuffleData(dataSet, dataSize);
+
+    // Podział na zbiór treningowy i testowy (80% do 20%)
+    int trainingSize = dataSize * 0.8;
+    int testingSize = dataSize - trainingSize;
+
+    // Przypisanie danych do zbioru treningowego i testowego
+    DataPoint *trainingSet = dataSet;
+    DataPoint *testingSet = dataSet + trainingSize;
+
+    // Inicjalizacja 10 perceptronów z losowymi wagami
+    Perceptron perceptrons[NUM_PERCEPTRONS];
+    #pragma omp parallel for
+    for (int p = 0; p < NUM_PERCEPTRONS; p++) {
+        for (int i = 0; i < FEATURES; i++) {
+            perceptrons[p].weights[i] = (rand() % 1000) / 1000.0;  // Losowe liczby z przedziału [0, 1)
+        }
+        perceptrons[p].bias = (rand() % 1000) / 1000.0;
+    }
+
+    // Uczenie każdego perceptronu na odpowiednim fragmencie zbioru treningowego przez określoną liczbę epok
+    double learningRate = 0.01;
+    int numEpochs = 1000;
+
+    #pragma omp parallel for collapse(2)
+    for (int epoch = 0; epoch < numEpochs; epoch++) {
+        for (int p = 0; p < NUM_PERCEPTRONS; p++) {
+            for (int i = 0; i < trainingSize / NUM_PERCEPTRONS; i++) {
+                trainPerceptron(&perceptrons[p], trainingSet[i + p * (trainingSize / NUM_PERCEPTRONS)], learningRate);
+            }
+        }
+    }
+
+    printf("Time: %f\n", omp_get_wtime() - start);
+
+    // Testowanie każdego perceptronu na zbiorze testowym
+    int correctPredictions = 0;
+    printf("Real Label\tPredicted Label\tEvaluation\n");
+    #pragma omp parallel for reduction(+:correctPredictions)
+    for (int i = 0; i < testingSize; i++) {
+        int realLabel = testingSet[i].label;
+        int majorityVote = 0;
+
+        for (int p = 0; p < NUM_PERCEPTRONS; p++) {
+            majorityVote += predict(perceptrons[p], testingSet[i].features);
+        }
+
+        majorityVote = (majorityVote >= (NUM_PERCEPTRONS / 2)) ? 1 : 0;
+
+        printf("%d\t\t%d\t\t", realLabel, majorityVote);
+
+        if (realLabel == majorityVote) {
+            printf("Correct\n");
+            correctPredictions++;
+        } else {
+            printf("Incorrect\n");
+        }
+    }
+
+    double accuracy = (double)correctPredictions / testingSize * 100.0;
+    printf("Accuracy: %.2f%%\n", accuracy);
+
+    free(dataSet);
+
+    return 0;
+}
--- test.c
+++ test.c
@@ -2,24 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-#include <mpi.h>
+#include <omp.h>
 
-#define FEATURES 8
 
+#define FEATURES 8  // Liczba cech (atrybutów) w danych
+#define NUM_PERCEPTRONS 10
+
+// Struktura reprezentująca pojedynczy punkt danych
 typedef struct {
     double features[FEATURES];
     int label;
 } DataPoint;
 
+// Struktura reprezentująca perceptron
 typedef struct {
     double weights[FEATURES];
     double bias;
 } Perceptron;
 
+// Funkcja sigmoidalna
 double sigmoid(double x) {
     return 1.0 / (1.0 + exp(-x));
 }
 
+// Funkcja ucząca perceptron na pojedynczym punkcie danych
 void trainPerceptron(Perceptron *perceptron, DataPoint data, double learningRate) {
     double weightedSum = perceptron->bias;
     for (int i = 0; i < FEATURES; i++) {
@@ -36,6 +42,7 @@ void trainPerceptron(Perceptron *perceptron, DataPoint data, double learningRate
     }
 }
 
+// Funkcja klasyfikująca dane za pomocą perceptronu
 int predict(Perceptron perceptron, double *features) {
     double weightedSum = perceptron.bias;
     for (int i = 0; i < FEATURES; i++) {
@@ -46,18 +53,19 @@ int predict(Perceptron perceptron, double *features) {
     return (prediction >= 0.5) ? 1 : 0;
 }
 
-void shuffleData(DataPoint *dataSet, int dataSize, int rank) {
-    if (rank == 0) {
-        for (int i = dataSize - 1; i > 0; i--) {
-            int j = rand() % (i + 1);
+// Funkcja do losowego tasowania danych
+void shuffleData(DataPoint *dataSet, int dataSize) {
+    for (int i = dataSize - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
 
-            DataPoint temp = dataSet[i];
-            dataSet[i] = dataSet[j];
-            dataSet[j] = temp;
-        }
+        // Zamień elementy i j miejscami
+        DataPoint temp = dataSet[i];
+        dataSet[i] = dataSet[j];
+        dataSet[j] = temp;
     }
 }
 
+// Funkcja do obliczania błędu na zbiorze danych
 double calculateError(Perceptron perceptron, DataPoint *dataSet, int dataSize) {
     double totalError = 0.0;
     for (int i = 0; i < dataSize; i++) {
@@ -67,10 +75,12 @@ double calculateError(Perceptron perceptron, DataPoint *dataSet, int dataSize) {
     return totalError / dataSize;
 }
 
+// Funkcja do normalizacji danych zgodnie ze standard scalerem
 void standardScaler(DataPoint *dataSet, int dataSize) {
     double mean[FEATURES] = {0.0};
     double variance[FEATURES] = {0.0};
 
+    // Oblicz średnią dla każdej cechy
     for (int i = 0; i < dataSize; i++) {
         for (int j = 0; j < FEATURES; j++) {
             mean[j] += dataSet[i].features[j];
@@ -81,6 +91,7 @@ void standardScaler(DataPoint *dataSet, int dataSize) {
         mean[j] /= dataSize;
     }
 
+    // Oblicz wariancję dla każdej cechy
     for (int i = 0; i < dataSize; i++) {
         for (int j = 0; j < FEATURES; j++) {
             variance[j] += pow(dataSet[i].features[j] - mean[j], 2);
@@ -92,6 +103,7 @@ void standardScaler(DataPoint *dataSet, int dataSize) {
         variance[j] = sqrt(variance[j]);
     }
 
+    // Zastosuj standard scaler do każdej cechy
     for (int i = 0; i < dataSize; i++) {
         for (int j = 0; j < FEATURES; j++) {
             dataSet[i].features[j] = (dataSet[i].features[j] - mean[j]) / variance[j];
@@ -99,25 +111,21 @@ void standardScaler(DataPoint *dataSet, int dataSize) {
     }
 }
 
-int main(int argc, char *argv[]) {
-    MPI_Init(&argc, &argv);
-
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-
+int main() {
+    // Inicjalizacja generatora liczb losowych
     srand(42);
 
     FILE *file = fopen("diab.csv", "r");
     if (file == NULL) {
         perror("Unable to open file");
-        MPI_Finalize();
         return 1;
     }
 
-    int dataSize = 768;
+    // Wczytanie danych z pliku
+    int dataSize = 768;  // Załóżmy, że plik zawiera 768 rekordów danych
     DataPoint *dataSet = malloc(dataSize * sizeof(DataPoint));
 
+    // Pominięcie pierwszej linii (nagłówek)
     char buffer[256];
     fgets(buffer, sizeof(buffer), file);
 
@@ -130,65 +138,72 @@ int main(int argc, char *argv[]) {
 
     fclose(file);
 
-    double start = MPI_Wtime();
+    int start = omp_get_wtime();
 
+    // Normalizacja danych zgodnie ze standard scalerem
     standardScaler(dataSet, dataSize);
 
-    shuffleData(dataSet, dataSize, rank);
+    // Tasowanie danych
+    shuffleData(dataSet, dataSize);
 
+    // Podział na zbiór treningowy i testowy (80% do 20%)
     int trainingSize = dataSize * 0.8;
     int testingSize = dataSize - trainingSize;
 
-    int localTrainingSize = trainingSize / size;
-    int localTrainingStart = rank * localTrainingSize;
-    int localTrainingEnd = (rank == size - 1) ? trainingSize : (rank + 1) * localTrainingSize;
+    // Przypisanie danych do zbioru treningowego i testowego
+    DataPoint *trainingSet = dataSet;
+    DataPoint *testingSet = dataSet + trainingSize;
 
-    DataPoint *localTrainingSet = dataSet + localTrainingStart;
-
-    Perceptron perceptron;
-
-    for (int i = 0; i < FEATURES; i++) {
-        perceptron.weights[i] = (rand() % 1000) / 1000.0;
+    // Inicjalizacja 10 perceptronów z losowymi wagami
+    Perceptron perceptrons[NUM_PERCEPTRONS];
+    for (int p = 0; p < NUM_PERCEPTRONS; p++) {
+        for (int i = 0; i < FEATURES; i++) {
+            perceptrons[p].weights[i] = (rand() % 1000) / 1000.0;  // Losowe liczby z przedziału [0, 1)
+        }
+        perceptrons[p].bias = (rand() % 1000) / 1000.0;
     }
-    perceptron.bias = (rand() % 1000) / 1000.0;
 
+    // Uczenie każdego perceptronu na odpowiednim fragmencie zbioru treningowego przez określoną liczbę epok
     double learningRate = 0.01;
     int numEpochs = 1000;
 
     for (int epoch = 0; epoch < numEpochs; epoch++) {
-        for (int i = localTrainingStart; i < localTrainingEnd; i++) {
-            trainPerceptron(&perceptron, localTrainingSet[i], learningRate);
-        }
-
-        MPI_Allreduce(MPI_IN_PLACE, perceptron.weights, FEATURES, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-        MPI_Allreduce(MPI_IN_PLACE, &perceptron.bias, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-    }
-
-    int correctPredictions = 0;
-
-    if (rank == 0) {
-        printf("Time: %f\n", MPI_Wtime() - start);
-        printf("Real Label\tPredicted Label\tEvaluation\n");
-        for (int i = trainingSize; i < dataSize; i++) {
-            int realLabel = dataSet[i].label;
-            int predictedLabel = predict(perceptron, dataSet[i].features);
-
-            printf("%d\t\t%d\t\t", realLabel, predictedLabel);
-            if (realLabel == predictedLabel) {
-                printf("Correct\n");
-                correctPredictions++;
-            } else {
-                printf("Incorrect\n");
+        for (int p = 0; p < NUM_PERCEPTRONS; p++) {
+            for (int i = 0; i < trainingSize / NUM_PERCEPTRONS; i++) {
+                trainPerceptron(&perceptrons[p], trainingSet[i + p * (trainingSize / NUM_PERCEPTRONS)], learningRate);
             }
         }
-
-        double accuracy = (double)correctPredictions / testingSize * 100.0;
-        printf("Accuracy: %.2f%%\n", accuracy);
-        printf("Accuracy: %.2f%%\n", accuracy);
     }
 
+    printf("Time: %f\n", omp_get_wtime() - start);
+
+    // Testowanie każdego perceptronu na zbiorze testowym
+    int correctPredictions = 0;
+    printf("Real Label\tPredicted Label\tEvaluation\n");
+    for (int i = 0; i < testingSize; i++) {
+        int realLabel = testingSet[i].label;
+        int majorityVote = 0;
+
+        for (int p = 0; p < NUM_PERCEPTRONS; p++) {
+            majorityVote += predict(perceptrons[p], testingSet[i].features);
+        }
+
+        majorityVote = (majorityVote >= (NUM_PERCEPTRONS / 2)) ? 1 : 0;
+
+        printf("%d\t\t%d\t\t", realLabel, majorityVote);
+
+        if (realLabel == majorityVote) {
+            printf("Correct\n");
+            correctPredictions++;
+        } else {
+            printf("Incorrect\n");
+        }
+    }
+
+    double accuracy = (double)correctPredictions / testingSize * 100.0;
+    printf("Accuracy: %.2f%%\n", accuracy);
+
     free(dataSet);
-    MPI_Finalize();
 
     return 0;
 }
